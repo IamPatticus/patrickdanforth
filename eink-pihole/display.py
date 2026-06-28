88301e9acaca928bb9f0b962eab51352b3e5564a
Talos Sykeus <talossykeus@gmail.com>
2026-06-28 22:09:09 +0000

    eink-pihole: revert to white-frame clear before content

--- eink-pihole/display.py
+++ eink-pihole/display.py
@@ -146,12 +146,10 @@ def wrap_text(text, font, max_width):
 def update_display(image):
     epd = epd2in13_V4.EPD()
     epd.init()
-    # Aggressive clear sequence to remove ghosting/red dots
-    epd.Clear(0xFF)  # clear to white
-    epd.display(epd.getbuffer(Image.new('1', (WIDTH, HEIGHT), 0)))  # black
-    epd.delay_ms(400)
-    epd.display(epd.getbuffer(Image.new('1', (WIDTH, HEIGHT), 255)))  # white
-    epd.delay_ms(400)
+    # Clear to white first, then draw content
+    white = Image.new('1', (WIDTH, HEIGHT), 255)
+    epd.display(epd.getbuffer(white))
+    epd.delay_ms(200)
     epd.display(epd.getbuffer(image))
     epd.sleep()
 
