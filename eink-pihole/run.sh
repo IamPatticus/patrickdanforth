b0c60429df5ed7bf5c188b3592cbd30aad2be0cd
Talos Sykeus <talossykeus@gmail.com>
2026-06-28 19:49:30 +0000

    eink-pihole: use venv python3 directly in run.sh

--- eink-pihole/run.sh
+++ eink-pihole/run.sh
@@ -2,6 +2,5 @@
 # Run the E-Ink Pi-hole display once.
 set -e
 cd "$(dirname "$0")"
-source ./venv/bin/activate
 export PIHOLE_URL="${PIHOLE_URL:-http://192.168.1.203}"
-python3 display.py
+./venv/bin/python3 display.py
